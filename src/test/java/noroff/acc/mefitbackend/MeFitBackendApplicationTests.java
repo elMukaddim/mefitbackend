1988cc1d8e22f5c518523c265fec8750872e126a
HIlmi <hilmi_g@live.nl>
2022-06-04 19:12:07 +0100

    Getting started

---
+++ src/test/java/noroff/acc/mefitbackend/MeFitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package noroff.acc.mefitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeFitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
